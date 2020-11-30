1b63d7a6dbcd2f6969d223b425752db80e7f1677
Jules Rouault <trustprinciples@gmail.com>
2020-11-30 15:40:10 +0100

    Tu vas l'avoir, ton Singletonton

---
+++ Asynchrone/Assets/Scripts/IA/Editor/IA_FOVEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(anAI))]
+public class IA_FOVEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        anAI myAI = (anAI)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(myAI.transform.position, Vector3.up, Vector3.forward, 360, myAI.ViewRadius);
+        Vector3 ViewAngleA = myAI.DirFromAngle(-myAI.ViewAngle / 2, false);
+        Vector3 ViewAngleB = myAI.DirFromAngle(myAI.ViewAngle / 2, false);
+
+        Handles.DrawLine(myAI.transform.position, myAI.transform.position + ViewAngleA * myAI.ViewRadius);
+        Handles.DrawLine(myAI.transform.position, myAI.transform.position + ViewAngleB * myAI.ViewRadius);
+
+        Handles.color = Color.red;
+    }
+}
---
+++ Asynchrone/Assets/Scripts/IA/anAI.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class anAI : MonoBehaviour
+{
+    [Header("Composants")]
+    NavMeshAgent myNavMeshAgent;
+
+    [Header("TEST: Cible Manuelle")]
+    public Transform Target;
+
+    [Header("Champ de vision")]
+    public float ViewRadius;
+    [Range(0,360)]
+    public float ViewAngle;
+    [Space]
+    public LayerMask CiblesMask;
+    public LayerMask ObstacleMask;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        myNavMeshAgent = GetComponent<NavMeshAgent>();
+        myNavMeshAgent.SetDestination(Target.position);
+    }
+
+    void FindVisibleTargets()
+    {
+        Collider[] targetInViewRadius = Physics.OverlapSphere(transform.position, ViewRadius, CiblesMask);
+
+        for (int i = 0; i < targetInViewRadius.Length; i++)
+        {
+            Transform theTarget = targetInViewRadius[i].transform;
+
+            Vector3 dirToTarget = (theTarget.position - transform.position).normalized;
+            if(Vector3.Angle(transform.forward, dirToTarget) < ViewAngle / 2)
+            {
+                float dstToTarget = Vector3.Distance(transform.position, theTarget.position);
+
+                if(!Physics.Raycast(transform.position, dirToTarget, dstToTarget, ObstacleMask))
+                {
+
+                }
+            }
+        }
+    }
+
+    public Vector3 DirFromAngle(float AngleInDegrees, bool AngleisGlobal)
+    {
+        if (!AngleisGlobal)
+            AngleInDegrees += transform.eulerAngles.y;
+        return new Vector3(Mathf.Sin(AngleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(AngleInDegrees * Mathf.Deg2Rad));
+    }
+}
