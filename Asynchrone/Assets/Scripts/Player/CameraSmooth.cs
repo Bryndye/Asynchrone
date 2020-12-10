ba7a13d40234e9fad7344aa8d2b79d2d9ae1fa1c
Jules Rouault <trustprinciples@gmail.com>
2020-12-10 14:45:51 +0100

    CE BON LA CAMERA EL VA BIEN

--- Asynchrone/Assets/Scripts/Player/CameraSmooth.cs
+++ Asynchrone/Assets/Scripts/Player/CameraSmooth.cs
@@ -6,14 +6,14 @@ public class CameraSmooth : Singleton<CameraSmooth>
 {
     public Transform Target;
 
-    void FixedUpdate() => SmoothFollow();
+    void Update() => SmoothFollow();
 
     void SmoothFollow()
     {
         if (Target != null)
         {
             Vector3 smooth = new Vector3(Target.position.x, 1, Target.position.z) - transform.position;
-            transform.position += smooth / 20;
+            transform.position += smooth / 40;
         }
     }
 
