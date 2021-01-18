4c768d27889864a46b730ce9a81a833e9d90d8fb
CluClu <hugocluzel17@hotmail.fr>
2021-01-18 15:59:43 +0100

    CLEAR MOI CA
    jai enleve mes sscripts KK en surplus et on peut quitter une scene ECHAP et retourner au menu

---
+++ Asynchrone/Assets/MenuInGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInGame : Singleton<MenuInGame>
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    void Update()
+    {
+        Scene sc = SceneManager.GetActiveScene();
+        if (Input.GetKeyDown(KeyCode.Escape) && sc.buildIndex != 0)
+        {
+            SceneManager.LoadScene(0);
+            //Debug.Log("Back to menu");
+        }
+    }
+}
