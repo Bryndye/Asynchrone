d8332a26c3cba945a752decbe05b4fa6f31aa4f8
CluClu <hugocluzel17@hotmail.fr>
2021-05-07 12:00:19 +0200

    Pince

---
+++ Asynchrone/Assets/PinceSetPos.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinceSetPos : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    private void OnDrawGizmosSelected()
+    {
+        if (target == null)
+        {
+            return;
+        }
+        transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
+    }
+}
