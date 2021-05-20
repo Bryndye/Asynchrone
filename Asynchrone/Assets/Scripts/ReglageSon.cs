b4cb88b2c1948224cbc716bc932dc0bbf68653f5
CluClu <hugocluzel17@hotmail.fr>
2021-05-20 16:07:36 +0200

    Menu InGame sans Bug
    On peut bouger la slide et ca save la valeur mais ne change pas le volume

--- Asynchrone/Assets/Scripts/ReglageSon.cs
+++ Asynchrone/Assets/Scripts/ReglageSon.cs
@@ -12,7 +12,6 @@ public class ReglageSon : MonoBehaviour
 
     private void Start()
     {
-        Debug.Log(PlayerPrefs.GetFloat("musicVolume"));
         OnChangeMusic(PlayerPrefs.GetFloat("musicVolume"));
     }
 
