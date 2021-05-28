9fdb7c3098e8c962fb6b00694422db4718ad0d8c
CluClu <hugocluzel17@hotmail.fr>
2021-05-28 17:50:23 +0200

    recnontre ta mere du 3eme type

---
+++ Asynchrone/Assets/RencontreManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RencontreManager : MonoBehaviour
+{
+    public Animator animRbt;
+    public Animator animHm;
+    private int indexOfNextlevel;
+
+    CanvasManager cm
+    {
+        get
+        {
+            return CanvasManager.Instance;
+        }
+    }
+
+    SoundManager SM
+    {
+        get
+        {
+            return SoundManager.Instance;
+        }
+    }
+    MusicManager MM
+    {
+        get
+        {
+            return MusicManager.Instance;
+        }
+    }
+
+    private void OnEnable()
+    {
+        animHm.SetTrigger("load");
+        animRbt.SetTrigger("load");
+
+        Invoke(nameof(LoadLevel), 2f);
+    }
+
+    private void LoadLevel()
+    {
+        PlayerPrefs.SetInt("indexLevel", indexOfNextlevel);
+
+        cm.anim.SetTrigger("Transition");
+        SM.GetASound("Ascenseur_Fermeture", transform);
+        MM.CloseMusic();
+
+        SceneManager.LoadScene(7);
+    }
+
+}
