1a3e2547c554b86dbed696614beb423828ec0061
CluClu <hugocluzel17@hotmail.fr>
2021-06-14 11:30:54 +0200

    Notice Here

---
+++ Asynchrone/Assets/EventNotice.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventNotice : MonoBehaviour
+{
+    [SerializeField] private GameObject noticeActive;
+    [SerializeField] private Image noticeImage;
+
+    private void Start()
+    {
+        noticeImage.gameObject.SetActive(false);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //done
+        noticeActive.SetActive(true);
+    }
+
+    public void ActiveNotice()
+    {
+        noticeImage.gameObject.SetActive(!noticeImage.gameObject.activeSelf);
+    }
+}
